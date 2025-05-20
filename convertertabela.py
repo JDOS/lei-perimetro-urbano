dacc677b416fddec2b8e8db91a4996715e67a487
Jônatas Silvério <daniel_jhonny@hotmail.com>
2025-05-20 20:01:11 +0000

    update numeração e entrada de dados

--- convertertabela.py
+++ convertertabela.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from docx import Document
 
+#MUDAR VALORES AQUI
+INICIO = 552
+FIM = 1233
+
+
 def lerarquivoxlxs(nomearquivo):
     # Leitura do arquivo .xlsx
     df = pd.read_excel(nomearquivo, engine='openpyxl')  
@@ -33,8 +38,8 @@ df = lerarquivoxlxs("Vertices.xlsx")
 print(df.head(18))
 #print(df.tail())
 
-inicio = 0
-fim = 1
+inicio = 19
+fim = 135
 print("TESTE PADRÕES")
 for n in range(inicio,fim):
     print(formatObjectID(df.loc[n, "OBJECTID_1"]))
@@ -45,13 +50,13 @@ for n in range(inicio,fim):
 
 
 
-inicio = 0
-fim = 18
+inicio = INICIO-1
+fim = FIM-1
 df = lerarquivoxlxs("Vertices.xlsx")
 print(df.head(fim))
 # Cria o documento Word
 doc = Document()
-doc.add_heading("Sede Urbana "+str(inicio)+" até "+str(fim), level=1)
+doc.add_heading("Sede Urbana "+formatObjectID(df.loc[inicio, "OBJECTID_1"])+" até "+formatObjectID(df.loc[fim, "OBJECTID_1"]), level=1)
 paragrafo = doc.add_paragraph()
 
 
@@ -61,17 +66,17 @@ paragrafo.add_run(textoinicio)
 texto = formatObjectID(df.loc[inicio, "OBJECTID_1"]) + ", " + "de coordenadas N "+formatCoords(df.loc[inicio, "N"]) +" e E "+formatCoords(df.loc[inicio, "E"])+", que segue confrontando por linha seca em um azimute de "+ formatDirection(df.loc[inicio, "Direction"]) + " a uma distância de "+formatDistance(df.loc[inicio, "Distance"])+" até o vértice "
 paragrafo.add_run(texto)
 
-for n in range(inicio+1,fim-1):
+for n in range(inicio+1,fim+1):
     texto = formatObjectID(df.loc[n, "OBJECTID_1"]) + "; "
     paragrafo.add_run(texto)
     texto = "Do vértice " + formatObjectID(df.loc[n, "OBJECTID_1"]) + ", " + "de coordenadas N "+formatCoords(df.loc[n, "N"]) +" e E "+formatCoords(df.loc[n, "E"])+", que segue confrontando por linha seca em um azimute de "+ formatDirection(df.loc[n, "Direction"]) + " a uma distância de "+formatDistance(df.loc[n, "Distance"])+" até o vértice "
     paragrafo.add_run(texto)
 
 #Adiciona último texto
-texto =  " retornando ao vértice " + formatObjectID(df.loc[fim, "OBJECTID_1"]) +", onde teve início essa descrição."
+texto =  " retornando ao vértice " + formatObjectID(df.loc[inicio, "OBJECTID_1"]) +", onde teve início essa descrição."
 paragrafo.add_run(texto)
 
 #Salva o documento
-doc.save("saida.docx")
+doc.save("saida-"+formatObjectID(df.loc[inicio, "OBJECTID_1"])+"-"+formatObjectID(df.loc[fim, "OBJECTID_1"])+".docx")
 
 #produção de texto
